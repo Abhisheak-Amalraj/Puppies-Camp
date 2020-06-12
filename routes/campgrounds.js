e05a16c7550702c7df0ac4269b8e7f6452bdf32f
Abhisheak-Amalraj <64534752+Abhisheak-Amalraj@users.noreply.github.com>
2020-06-12 19:32:26 +0530

    Add files via upload

---
+++ routes/campgrounds.js
@@ -0,0 +1,121 @@
+var express = require("express");
+var router  = express.Router();
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+var middleware = require("../middleware");
+
+// Define escapeRegex function for search feature
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
+
+//INDEX - show all campgrounds
+router.get("/", function(req, res){
+  if(req.query.search && req.xhr) {
+      const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+      // Get all campgrounds from DB
+      Campground.find({name: regex}, function(err, allCampgrounds){
+         if(err){
+            console.log(err);
+         } else {
+            res.status(200).json(allCampgrounds);
+         }
+      });
+  } else {
+      // Get all campgrounds from DB
+      Campground.find({}, function(err, allCampgrounds){
+         if(err){
+             console.log(err);
+         } else {
+            if(req.xhr) {
+              res.json(allCampgrounds);
+            } else {
+              res.render("campgrounds/index",{campgrounds: allCampgrounds, page: 'campgrounds'});
+            }
+         }
+      });
+  }
+});
+
+//CREATE - add new campground to DB
+router.post("/", middleware.isLoggedIn, function(req, res){
+  // get data from form and add to campgrounds array
+  var name = req.body.name;
+  var image = req.body.image;
+  var desc = req.body.description;
+  var author = {
+      id: req.user._id,
+      username: req.user.username
+  }
+  var cost = req.body.cost;
+    var newCampground = {name: name, image: image, description: desc, cost: cost, author:author};
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            console.log(newlyCreated);
+            res.redirect("/campgrounds");
+        }
+    });
+  });
+//NEW - show form to create new campground
+router.get("/new", middleware.isLoggedIn, function(req, res){
+   res.render("campgrounds/new"); 
+});
+
+// SHOW - shows more info about one campground
+router.get("/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+          console.log(err);
+        } else {
+          console.log(foundCampground)
+          //render show template with that campground
+          res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+router.get("/:id/edit", middleware.checkUserCampground, function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            //render show template with that campground
+            res.render("campgrounds/edit", {campground: foundCampground});
+        }
+    });
+});
+
+router.put("/:id", function(req, res){
+    var newData = {name: req.body.name, image: req.body.image, description: req.body.description, cost: req.body.cost};
+    Campground.findByIdAndUpdate(req.params.id, {$set: newData}, function(err, campground){
+        if(err){
+            req.flash("error", err.message);
+            res.redirect("back");
+        } else {
+            req.flash("success","Successfully Updated!");
+            res.redirect("/campgrounds/" + campground._id);
+        }
+    });
+  });
+
+router.delete("/:id", function(req, res) {
+  Campground.findByIdAndRemove(req.params.id, function(err, campground) {
+    Comment.remove({
+      _id: {
+        $in: campground.comments
+      }
+    }, function(err, comments) {
+      req.flash('error', campground.name + ' deleted!');
+      res.redirect('/campgrounds');
+    })
+  });
+});
+
+module.exports = router;
+
